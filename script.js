5f228feefe3f07e6041d6958027f413815701d3c
Drywock <7tt412@gmail.com>
2020-03-11 18:46:01 +0100

    Complit part 2 task 5

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-//  Task 4 :
-let image = document.createElement('img');
-image.setAttribute('src','flower2.png')
-document.getElementById('new_element').appendChild(image);
+function init() {
+    // Task 4:
+    let image = document.createElement('img');
+    image.setAttribute('src','flower2.png')
+    document.getElementById('new_element').appendChild(image);
+
+    // Task 5:
+    let spans = document.getElementById('rainbow').getElementsByTagName("span");
+    let colors = ["red", "orange", "yellow", "green", "blue", "purple", "pink"];
+
+    for(var i = spans.length - 1; i >= 0; i--) {
+        spans[i].style.color = colors [i % colors.length];
+    }
+}

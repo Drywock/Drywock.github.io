7b37a14730bb8673230b33bfad3c9d19596e739a
Drywock <7tt412@gmail.com>
2020-03-11 18:57:53 +0100

    Complit part 2 task 6

--- script.js
+++ script.js
@@ -11,4 +11,17 @@ function init() {
     for(var i = spans.length - 1; i >= 0; i--) {
         spans[i].style.color = colors [i % colors.length];
     }
+
+    // Task 6:
+    var changeSrc = function(event) {
+        if (event.target.src) {
+            let filename = event.target.src.replace(/^.*[\\\/]/, '');
+            if(filename === "flower1.png")
+                event.target.src = "flower2.png";
+            else 
+                event.target.src = "flower1.png";
+        }
+    };
+      
+    document.getElementById("event").addEventListener("mouseover", changeSrc);      
 }
